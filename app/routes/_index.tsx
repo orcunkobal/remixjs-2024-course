d49600efca36b6023f6f8ba56205c1d5009f414b
Orcun Kobal <orcunk@powergroup.com.tr>
2024-04-09 15:50:52 +0300

    route mobulleri bolum 2 first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,33 +1,3 @@
-import type { LoaderFunction, MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = (context) => {
-	console.log(context)
-	const {data,matches} = context
-	const rootData = matches.find(route => route.id === "root").data
-
-	let keywords = rootData.meta.keywords
-
-	if( data.meta?.keywords )
-		keywords = data.meta.keywords
-
-	return [
-		{ title: data.meta.title },
-		{ name: "description", content: data.meta.description },
-		{ name: "keywords", content: keywords },
-	];
-};
-
-
-export const loader:LoaderFunction = () => {
-	//api istedği yaptık
-	const data = {
-		meta:{
-			title:"Remix Kursu 2024",
-			description:"Remix kursuna hoşgeldin"
-		}
-	}
-	return data
-}
 
 export default function Index() {
 	return (
