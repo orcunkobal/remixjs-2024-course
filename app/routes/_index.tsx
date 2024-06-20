e2c5e00895c7e7d5bdd03bc4ac9563bfa30867d5
Orcun Kobal <orcunk@powergroup.com.tr>
2024-06-20 14:28:21 +0300

    link prefetch final

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
+import { Link } from "@remix-run/react";
 
 export default function Index() {
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8", display: "flex",flexDirection:"column" }}>
-			anasayfa
+			<div style={{height:2000,background:"gray"}}></div>
+			<Link to="/hakkimizda/" prefetch="viewport">Hakkımızda</Link>
 		</div>
 	);
 }
